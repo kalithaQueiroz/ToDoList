791ee2e1a6de49b79a5b8a382f796cd2b59d582c
kalithaQueiroz <kalitaqueiroz@gmail.com>
2024-04-24 16:59:53 -0300

    Adding comments to JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Seleção de Elemento
+// Seleção de Elementos
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
@@ -8,40 +8,46 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funcões
 const saveTodo = (text) => {
+    // Cria um elemento de div para representar a tarefa
     const todo = document.createElement("div");
     todo.classList.add("todo");
 
+    // Cria um elemento de título para a tarefa
     const todoTitle = document.createElement("h3");
     todoTitle.innerText = text;
     todo.appendChild(todoTitle);
 
+    // Cria um botão para marcar a tarefa como concluída
     const doneBtn = document.createElement("button");
     doneBtn.classList.add("finish-todo");
     doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
     todo.appendChild(doneBtn);
 
+    // Cria um botão para editar a tarefa
     const editBtn = document.createElement("button");
     editBtn.classList.add("edit-todo");
     editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
     todo.appendChild(editBtn);
 
+    // Cria um botão para remover a tarefa
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("remove-todo");
     deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     todo.appendChild(deleteBtn);
 
-    todoList.appendChild(todo);
-    todoInput.value = "";
-    todoInput.focus();
+    
+    todoList.appendChild(todo); // Adiciona a tarefa à lista de tarefas
+    todoInput.value = ""; // Limpa o campo de entrada
+    todoInput.focus(); // Volta a seleção para o campo de digitação
 };
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const inputValue = todoInput.value;
-
+    e.preventDefault(); // Impede o envio do formulário
+    
+    const inputValue = todoInput.value; // Obtém o valor do campo de entrada
+    
     if(inputValue) {
-        saveTodo(inputValue);
+        saveTodo(inputValue); // Salva a tarefa na lista
     }
 });
